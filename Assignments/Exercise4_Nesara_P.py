b6ff1248b3aa3016af8137d8d0b6202d224980b7
Nesara Pattanaphan <nesara.pattanaphan@gmail.com>
2025-12-27 21:56:59 +0700

    Create Exercise4_Nesara_P.py

---
+++ Assignments/Exercise4_Nesara_P.py
@@ -0,0 +1,18 @@
+'''
+Foundation English
+General Business
+Introduction to Computer Systems
+Computer Programming
+'''
+
+FE_Scr = float(76.5)
+GB_Scr = float(75.0)
+ICS_Scr = float(80.5)
+CP_Scr = float(88.0)
+
+
+print("----------------- Your Score ----------------")
+print("Foundation English = ", FE_Scr)
+print("General Business = ", GB_Scr)
+print("Introduction to Computer Systems = ",ICS_Scr)
+print("Computer Programming = ", CP_Scr)
