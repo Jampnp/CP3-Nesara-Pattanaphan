bee0cb7aad31b7eb074f965242113ea0038c1050
Nesara Pattanaphan <nesara.pattanaphan@gmail.com>
2026-01-04 14:31:08 +0700

    Create Lecture50_Nesara_P.py

---
+++ Assignments/Lecture50_Nesara_P.py
@@ -0,0 +1,16 @@
+def addNumber(x,y):
+    print(x+y)
+
+def minusNumber(x,y):
+    print(x-y)
+
+def timesNumber(x,y):
+    print(x*y)
+
+def divideNumer(x,y):
+    print(x/y)
+
+addNumber(1,5)
+minusNumber(5,3)
+timesNumber(2,3)
+divideNumer(10,2)
